ae1b392901636439396d7da315533eac7621c6a3
pyaephyokhant760 <pyaephyokhant2020dc@gmail.com>
2025-03-28 11:20:24 +0630

    lang_mm_and_el

---
+++ app/Http/Controllers/LanguageController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
+
+class LanguageController extends Controller
+{
+    public function language(Request $request)
+    {
+        $lang = $request->lang;
+        Session::put('locale', $lang);
+        App::setLocale(Session::get('locale'));
+        return back();
+    }
+}
